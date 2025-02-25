3de861deabacfc8a192fc45c6b46f557151c2fd7
Mai <mai@tilde.team>
2025-02-25 15:23:18 -0300

    packages(font): update `bedstead-nerd-font` 2.006 -> 3.251

--- packages/fonts/bedstead-nerd-font/default.nix
+++ packages/fonts/bedstead-nerd-font/default.nix
@@ -1,32 +1,29 @@
 { stdenvNoCC, fetchzip, nerd-font-patcher, lib, ... }:
-stdenvNoCC.mkDerivation rec {
+let version = "3.251";
+in stdenvNoCC.mkDerivation {
+  inherit version;
   pname = "bedstead-nerd-font";
-  version = "002.006";
 
   src = fetchzip {
     url = "https://bjh21.me.uk/bedstead/bedstead-${version}.zip";
-    hash = "sha256-N2EgkJb0AAcDL0K1XSliBYuOcjJkXgy+zZeb5PTG1Kw=";
+    hash = "sha256-tP4DVXaXvwMyGLhlzU7woeBlreD5UgTau3QQ/WlnjVo=";
   };
   nativeBuildInputs = [ nerd-font-patcher ];
   dontBuild = true;
 
   installPhase = ''
-    runHook preInstall
-
     mkdir -p $out/share/fonts/opentype
     for file in $src/*.otf; do 
       nerd-font-patcher -c -q --out "$out"/share/fonts/opentype $file 2> /dev/null
     done;
-
-    runHook postInstall
   '';
 
   meta = let inherit (lib) licenses platforms;
   in {
     description =
       "Bedstead is a family of outline fonts based on the characters produced by the Mullard SAA5050 series of Teletext Character Generators.";
-    homepage = "https://bjh21.me.uk/bedstead/";
-    license = licenses.cc0; # according to COPYING file.
+    homepage = "https://bjh21.me.uk/bedstead";
+    license = licenses.cc0;
     platforms = platforms.unix;
   };
 }
